3300776218cf4f0aa80b50f340ab041dec75b243
Owner <ij5775wy@go.minneapolis.edu>
2020-04-08 23:25:50 -0500

    Add project files.

---
+++ Quiz Program/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Quiz_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.pnlAnswer = new System.Windows.Forms.Panel();
+            this.rdoFalse = new System.Windows.Forms.RadioButton();
+            this.rdoTrue = new System.Windows.Forms.RadioButton();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
+            this.pnlAnswer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(13, 13);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.ReadOnly = true;
+            this.txtQuestion.Size = new System.Drawing.Size(224, 20);
+            this.txtQuestion.TabIndex = 0;
+            this.txtQuestion.TabStop = false;
+            // 
+            // pnlAnswer
+            // 
+            this.pnlAnswer.Controls.Add(this.rdoFalse);
+            this.pnlAnswer.Controls.Add(this.rdoTrue);
+            this.pnlAnswer.Location = new System.Drawing.Point(13, 40);
+            this.pnlAnswer.Name = "pnlAnswer";
+            this.pnlAnswer.Size = new System.Drawing.Size(224, 46);
+            this.pnlAnswer.TabIndex = 1;
+            this.pnlAnswer.TabStop = true;
+            // 
+            // rdoFalse
+            // 
+            this.rdoFalse.AutoSize = true;
+            this.rdoFalse.Location = new System.Drawing.Point(3, 26);
+            this.rdoFalse.Name = "rdoFalse";
+            this.rdoFalse.Size = new System.Drawing.Size(50, 17);
+            this.rdoFalse.TabIndex = 1;
+            this.rdoFalse.TabStop = true;
+            this.rdoFalse.Text = "False";
+            this.rdoFalse.UseVisualStyleBackColor = true;
+            // 
+            // rdoTrue
+            // 
+            this.rdoTrue.AutoSize = true;
+            this.rdoTrue.Location = new System.Drawing.Point(3, 3);
+            this.rdoTrue.Name = "rdoTrue";
+            this.rdoTrue.Size = new System.Drawing.Size(47, 17);
+            this.rdoTrue.TabIndex = 0;
+            this.rdoTrue.TabStop = true;
+            this.rdoTrue.Text = "True";
+            this.rdoTrue.UseVisualStyleBackColor = true;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(96, 132);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(47, 13);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "Score: ?";
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(64, 92);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(116, 23);
+            this.btnCheckAnswer.TabIndex = 4;
+            this.btnCheckAnswer.Text = "Check Answer";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 163);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnlAnswer);
+            this.Controls.Add(this.txtQuestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlAnswer.ResumeLayout(false);
+            this.pnlAnswer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Panel pnlAnswer;
+        private System.Windows.Forms.RadioButton rdoFalse;
+        private System.Windows.Forms.RadioButton rdoTrue;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnCheckAnswer;
+    }
+}
+
---
+++ Quiz Program/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Program
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly SortedList<String, bool> Questions = new SortedList<String, bool>()
+        {
+            {"SSD are faster than HDD", true},
+            {"Microsoft owns GitHub", true},
+            {"100 is the ASCII code for 'A'", false}
+        };
+
+        private int QuestionNumber = -1;
+        private int Score = 0;
+        private string incorrectAnswers = null; // The variable to hold all "incorrect answers"
+
+        public Form1()
+        {
+            InitializeComponent();
+            ShowNextQuestion();
+        }
+
+        private void ShowNextQuestion()
+        {
+            QuestionNumber++;
+
+            if (QuestionNumber < Questions.Count)
+            {
+                KeyValuePair<String, bool> question = Questions.ElementAt(QuestionNumber);
+                string questionText = question.Key;
+                txtQuestion.Text = questionText;
+
+                rdoTrue.Checked = false;
+                rdoFalse.Checked = false;
+            }
+
+            else
+            {
+                btnCheckAnswer.Enabled = false;
+                btnCheckAnswer.Text = "Quiz over!";
+
+                /* I used a nested if/else statement to reduce redundant code.
+                 * Is there a way I could've made this even more compact? */
+                if (Score == Questions.Count)
+                {
+                    MessageBox.Show($"Your score is {Score}", "Quiz Over!");
+                }
+
+                /* This pulls from the incorrectAnswers variable,
+                 * which displays the Key/Value for each incorrect answer.
+                 * I could almost certainly make it look better, but I didn't
+                 * want to spend too long on something that's currently trivial */
+                else
+                {
+                    MessageBox.Show($"Your score is {Score}\n\nIncorrect Answers{incorrectAnswers}", "Quiz Over!");
+                }
+            }
+        }
+
+        private void CheckAnswer()
+        {
+            if (QuestionNumber < Questions.Count)
+            {
+                KeyValuePair<String, bool> question = Questions.ElementAt(QuestionNumber);
+                bool correctAnswer = question.Value;
+
+                if (correctAnswer == true && rdoTrue.Checked == true)
+                {
+                    Score++;
+                }
+                
+                /* Changed this from an if to an if else so the next statement 
+                 * doesn't get forced if this statement's conditions don't get met */
+                else if (correctAnswer == false && rdoFalse.Checked == true)
+                {
+                    Score++;
+                }
+
+                /* This adds a new line and Key/Value pair to the incorrectAnswers
+                 * variable every time the user gets a question wrong */
+                else
+                {
+                    incorrectAnswers += $"\n{question}";
+                }
+
+                lblScore.Text = $"Score: {Score}";
+
+                /* I added this because it was impossible to tab back to the radio buttons
+                 * after answering the first question, despite having "Tab Stop" enabled.
+                 * Note: you can't tab back if you press tab before selecting an answer.
+                 * Instead, it'll only tab to the button. And if you press the arrow keys,
+                 * it'll select the text box. Because of this, I'd likely opt to use
+                 * a label if at all possible */
+                rdoTrue.Focus();
+            }
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+            if (rdoTrue.Checked || rdoFalse.Checked)
+            {
+                CheckAnswer();
+                ShowNextQuestion();
+            }
+        }
+    }
+}
